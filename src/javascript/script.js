5c11b42990afc23baa03109d4f9d824f55f63e6d
Kamil Gabrysiewicz <kamilgabrysiewicz60@gmail.com>
2022-03-25 02:52:37 +0100

    Skeleton of Homepage

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,3 +1,5 @@
 window.addEventListener('load', () => {
     document.getElementById("slider").style.height = `${document.getElementById("current-slide").offsetHeight}px`;
 })
+const maxHeight = window.innerHeight;
+document.getElementById("guitars").style.height = `${maxHeight}px`;
